ca7787f78b1d50a44d86112b67bd1d758ef49935
Johannes Schad <jmaschad@gmail.com>
2013-01-11 09:47:02 +0100

    Fixed a bug where offline clouds where not detected

--- src/de/jmaschad/storagesim/model/Disposer.scala
+++ src/de/jmaschad/storagesim/model/Disposer.scala
@@ -10,72 +10,77 @@ import de.jmaschad.storagesim.Log
 import org.cloudbus.cloudsim.core.CloudSim
 
 object Disposer {
-  val StatusInterval = 1
-  val CheckStatusInterval = 2 * StatusInterval
+    val StatusInterval = 1
+    val CheckStatusInterval = 2 * StatusInterval
 
-  val Base = 10100
-  val Shutdown = Base + 1
-  val MicroCloudStatus = Shutdown + 1
-  val MicroCloudShutdown = MicroCloudStatus + 1
-  val CheckStatus = MicroCloudShutdown + 1
-  val UserRequest = CheckStatus + 1
+    val Base = 10100
+    val Shutdown = Base + 1
+    val MicroCloudStatus = Shutdown + 1
+    val MicroCloudShutdown = MicroCloudStatus + 1
+    val CheckStatus = MicroCloudShutdown + 1
+    val UserRequest = CheckStatus + 1
 }
 
 class Disposer(name: String, distributor: RequestDistributor) extends SimEntity(name) {
-  private val statusTracker = new StatusTracker
-  private var eventHandler = onlineEventHandler _
+    private val statusTracker = new StatusTracker
+    private var eventHandler = onlineEventHandler _
 
-  private val log = Log.line("Disposer '%s'".format(getName), _: String)
+    private val log = Log.line("Disposer '%s'".format(getName), _: String)
 
-  override def startEntity(): Unit = {
-    send(getId(), Disposer.CheckStatusInterval, Disposer.CheckStatus)
-  }
+    override def startEntity(): Unit = {
+        send(getId(), Disposer.CheckStatusInterval, Disposer.CheckStatus)
+    }
 
-  override def shutdownEntity() = {}
+    override def shutdownEntity() = {}
 
-  override def processEvent(event: SimEvent): Unit = eventHandler(event)
+    override def processEvent(event: SimEvent): Unit = eventHandler(event)
 
-  private def onlineEventHandler(event: SimEvent): Unit = event.getTag() match {
-    case Disposer.Shutdown =>
-      log("shutdown now")
-      statusTracker.onlineClouds.keys.foreach(sendNow(_, MicroCloud.Shutdown))
-      eventHandler = offlineEventHandler _
+    private def onlineEventHandler(event: SimEvent): Unit = event.getTag() match {
+        case Disposer.Shutdown =>
+            log("shutdown now")
+            statusTracker.onlineClouds.keys.foreach(sendNow(_, MicroCloud.Shutdown))
+            eventHandler = offlineEventHandler _
 
-    case Disposer.MicroCloudStatus =>
-      val status = (event.getData() match {
-        case s: Status => s
-        case _ => throw new IllegalArgumentException
-      })
-      log("status update from %s: %s".format(sourceEntity(event), status))
-      statusTracker.online(event.getSource, status)
-      distributor.statusUpdate(statusTracker.onlineClouds)
+        case Disposer.MicroCloudStatus =>
+            val status = (event.getData() match {
+                case s: Status => s
+                case _ => throw new IllegalArgumentException
+            })
+            log("status update from %s: %s".format(sourceEntity(event), status))
+            statusTracker.online(event.getSource, status)
+            distributor.statusUpdate(statusTracker.onlineClouds)
 
-    case Disposer.MicroCloudShutdown =>
-      log("shutdown notification from %s".format(sourceEntity(event)))
-      statusTracker.offline(event.getSource)
+        case Disposer.MicroCloudShutdown =>
+            log("shutdown notification from %s".format(sourceEntity(event)))
+            statusTracker.offline(event.getSource)
 
-    case Disposer.CheckStatus =>
-      send(getId(), Disposer.CheckStatusInterval, Disposer.CheckStatus)
+        case Disposer.CheckStatus =>
+            send(getId(), Disposer.CheckStatusInterval, Disposer.CheckStatus)
+            val goneOffline = statusTracker.check()
+            if (goneOffline.nonEmpty) {
+                distributor.statusUpdate(statusTracker.onlineClouds)
+                log("detected offline cloud " + goneOffline.mkString(","))
+            }
 
-    case Disposer.UserRequest =>
-      val request = (event.getData() match {
-        case r: Request => r
-        case _ => throw new IllegalStateException
-      })
+        case Disposer.UserRequest =>
+            val request = (event.getData() match {
+                case r: Request => r
+                case _ => throw new IllegalStateException
+            })
 
-      distributor.selectMicroCloud(request) match {
-        case None =>
-          log("no cloud found for %s".format(request))
-          sendNow(event.getSource(), User.RequestFailed, request)
-        case Some(cloud) =>
-          log("foreward %s to %s".format(request, CloudSim.getEntity(cloud)))
-          sendNow(cloud, MicroCloud.UserRequest, request)
-      }
+            distributor.selectMicroCloud(request) match {
+                case None =>
+                    log("no cloud found for %s".format(request))
+                    sendNow(event.getSource(), User.RequestFailed, request)
+                case Some(cloud) =>
+                    log("foreward %s to %s".format(request, CloudSim.getEntity(cloud)))
+                    sendNow(cloud, MicroCloud.UserRequest, request)
+            }
 
-    case _ => log("dropped event " + event)
-  }
+        case _ => log("dropped event " + event)
+    }
 
-  private def sourceEntity(event: SimEvent) = CloudSim.getEntity(event.getSource())
+    private def sourceEntity(event: SimEvent) = CloudSim.getEntity(event.getSource())
 
-  private def offlineEventHandler(event: SimEvent) = log("Offline! dropped event " + event)
+    private def offlineEventHandler(event: SimEvent) = log("Offline! dropped event " + event)
 }
--- src/de/jmaschad/storagesim/model/StatusTracker.scala
+++ src/de/jmaschad/storagesim/model/StatusTracker.scala
@@ -4,27 +4,28 @@ import de.jmaschad.storagesim.model.microcloud.MicroCloud
 import de.jmaschad.storagesim.model.microcloud.Status
 
 class StatusTracker {
-  val MaxChecks = 2
-  var trackedClouds = Map.empty[Int, Int]
-  var cloudStatus = Map.empty[Int, Status]
+    val MaxChecks = 2
+    var trackedClouds = Map.empty[Int, Int]
+    var cloudStatus = Map.empty[Int, Status]
 
-  def onlineClouds: Map[Int, Status] = cloudStatus
+    def onlineClouds: Map[Int, Status] = cloudStatus
 
-  def online(microCloud: Int, status: Status): Unit = {
-    val missedCount = trackedClouds.getOrElse(microCloud, 0) - 1
-    trackedClouds = (trackedClouds - microCloud) + (microCloud -> missedCount.max(0))
-    cloudStatus += microCloud -> status
-  }
+    def online(microCloud: Int, status: Status): Unit = {
+        val missedCount = trackedClouds.getOrElse(microCloud, 0) - 1
+        trackedClouds = (trackedClouds - microCloud) + (microCloud -> missedCount.max(0))
+        cloudStatus += microCloud -> status
+    }
 
-  def offline(microCloud: Int): Unit = {
-    trackedClouds -= microCloud
-  }
+    def offline(microCloud: Int): Unit = {
+        trackedClouds -= microCloud
+    }
 
-  def check() = {
-    trackedClouds = trackedClouds.mapValues(_ + 1)
+    def check(): Iterable[Int] = {
+        trackedClouds = trackedClouds.mapValues(_ + 1)
 
-    val offline = trackedClouds.filter(_._2 >= MaxChecks)
-    trackedClouds --= offline.keys
-    cloudStatus --= offline.keys
-  }
+        val offline = trackedClouds.filter(_._2 >= MaxChecks).keys
+        trackedClouds --= offline
+        cloudStatus --= offline
+        offline
+    }
 }
