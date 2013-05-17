52869b340f2f8222416782f0d1fba82559f40340
Johannes Schad <jmaschad@gmail.com>
2013-05-17 14:04:29 +0200

    Fixed a dir name error

--- config/placement_25pop.scala
+++ config/placement_25pop.scala
@@ -1,7 +1,7 @@
 import de.jmaschad.storagesim._
 
 new StorageSimConfig {
-    outputDir = "experiments/placement_30pop"
+    outputDir = "experiments/placement_25pop"
     selector = PlacementBased()
     objectPopularityModel = ExponentialDist(0.25)
 }
